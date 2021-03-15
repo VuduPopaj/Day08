485e722839e96445ed282849e58a87917110af0a
VuduPopaj <adnanduljkovic@hotmail.com>
2021-03-16 00:55:48 +0100

    Ispravio ex10

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -9,12 +9,13 @@ function mathTest(x, y) {
   return Math.round(Math.pow(Math.sqrt(x) + Math.sqrt(y), 2));
 }
 // Only change code below this line
-console.log(mathTest(2, 2));
-console.log(mathTest(2, 2));
-console.log(mathTest(-2, 2));
-console.log(mathTest(2, -2));
-console.log(mathTest(2, 8));
-console.log(mathTest(3, 3));
-console.log(mathTest(0, 0));
+console.log(2, 2);
+console.log(2, 2);
+console.log(-2, 2);
+console.log(2, -2);
+console.log(2, 8);
+console.log(3, 3);
+console.log(0, 0);
+mathTest(-2, 2);
 // Only change code above this line
 module.exports = mathTest;
